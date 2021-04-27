8ab06686cd929f185dd8aabf7d32061ca459c626
Jeremie Madara <jeremiemadara@gmail.com>
2021-04-27 12:30:55 +0200

    Add components after a while

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,12 +1,30 @@
 import React from 'react';
 
-const Navigation = () => {
-     
+const Navigation = ({onRouteChange , isSignedIn}) => {
+    if(isSignedIn) {
         return (
             <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-                <p className='f3 link dim black underline pa3 pointer'>Sign Out</p>
+                <p 
+                className='f3 link dim black underline pa3 pointer'
+                onClick={() =>onRouteChange('signout')}
+                >Sign Out</p>
             </nav>   
-        )
+        ) } else {
+            return (
+                <div>
+                   <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+                        <p 
+                        className='f3 link dim black underline pa3 pointer'
+                        onClick={() =>onRouteChange('signin')}
+                        >Sign In</p>
+                        <p 
+                        className='f3 link dim black underline pa3 pointer'
+                        onClick={() =>onRouteChange('register')}
+                        >Register</p>
+                    </nav>  
+                    </div>
+)
+        }
 }
 
 export default Navigation;
